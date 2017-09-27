8434d08f45a14f8c78f78a8fb7267f9cbec84045
ximena <xime.huertas@rocketmail.com>
2017-09-26 22:33:41 -0500

    Initial commit

---
+++ app/src/main/java/com/i044114/taller2/Adapters/AdapterCountrySecond.java
@@ -0,0 +1,8 @@
+package com.i044114.taller2.Adapters;
+
+/**
+ * Created by ACER on 26/09/2017.
+ */
+
+public class AdapterCountrySecond {
+}
---
+++ app/src/main/java/com/i044114/taller2/Parser/JsonCountrySecond.java
@@ -0,0 +1,8 @@
+package com.i044114.taller2.Parser;
+
+/**
+ * Created by ACER on 26/09/2017.
+ */
+
+public class JsonCountrySecond {
+}
